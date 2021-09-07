e3c30f0d4b308966f856cb035981a1f5f03f0cd9
mjturizom <marlonturizom@gmail.com>
2021-09-07 10:43:52 -0500

    agrego carga de tipos desde API a DB al sincronizar modelos

--- api/index.js
+++ api/index.js
@@ -19,9 +19,16 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { default: axios } = require('axios');
+const {Type } =require('./src/db.js')
 
 // Syncing all the models at once.
 conn.sync().then(() => {
+  //Cargamos los tipos desde la API a DB cuando conectamos
+  axios.get('https://pokeapi.co/api/v2/type').then((tipos)=>{
+   Promise.all(tipos.data.results.map((tipo)=>Type.findOrCreate({where:{name: tipo.name}}))).then((values)=>{console.log('Tipos creados', JSON.stringify(values))})
+    
+  });
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
