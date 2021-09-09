5212f8d0871f1550e9a542fa021437cf2c78a07f
mjturizom <marlonturizom@gmail.com>
2021-09-08 20:27:17 -0500

    Add routes components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
 import './App.css';
+import {Route} from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <React.Fragment>
+    
+    <Route exact path="/" component={LandingPage} />
+    <Route exact path="/pokemons" component={Home} />
+    <Route path="/pokemons/:id" component={LandingPage} />
+</React.Fragment>
   );
 }
 
