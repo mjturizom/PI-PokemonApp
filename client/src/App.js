fa98b883cadcdf568654510891acafa6875582d0
mjturizom <marlonturizom@gmail.com>
2021-09-11 00:25:41 -0500

    Add routes and switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import './App.css';
-import {Route} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Home from "./components/Home/Home"
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <React.Fragment>
-    
-    <Route exact path="/" component={LandingPage} />
-    <Route exact path="/pokemons" component={Home} />
-    <Route path="/pokemons/:id" component={LandingPage} />
-</React.Fragment>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route path="/pokemons/:id" component={LandingPage} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
