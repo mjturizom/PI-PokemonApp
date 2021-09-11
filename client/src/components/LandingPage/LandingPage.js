7b7b368b529a81c0945162234e1306ade14344f1
mjturizom <marlonturizom@gmail.com>
2021-09-11 00:29:52 -0500

    modeling component

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
-import './LandingPage.css'
-
-export default function LandingPage(){
-    return (
-        <h1>LandingPage</h1>
-    )
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>LandingPage</h1>
+      <Link to="/home">
+        <button>Let's go!</button>
+      </Link>
+    </div>
+  );
 }
