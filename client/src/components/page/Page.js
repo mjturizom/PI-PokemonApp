567fb6d21adf84c0aae7d1b44153be75c3c82e02
mjturizom <marlonturizom@gmail.com>
2021-09-11 13:23:35 -0500

    Creating page component and files

---
+++ client/src/components/page/Page.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Page.css";
+export default function Page({ nPages, Page }) {
+  return (
+    <nav>
+      <ul className="pag">
+        {nPages &&
+          nPages.map((page) => {
+            return (
+              <li key={page} onClick={() => Page(page)}>
+                {page}
+              </li>
+            );
+          })}
+      </ul>
+    </nav>
+  );
+}
