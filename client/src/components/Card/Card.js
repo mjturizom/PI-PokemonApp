98d250f0790fcf33267c13a086d7c0f316ff8ad5
mjturizom <marlonturizom@gmail.com>
2021-09-11 00:30:46 -0500

    modeling component

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./Card.css";
 
-export default function Card({name, image , types}){
-return(
-    <div>
-        <h3>{name}</h3>
-        <h5>{image}</h5>
-        <img src={image} alt='Without img'/>
+export default function Card({ id, name, image, types }) {
+    let i=0;
+  return (
+    <div className="card">
+      <h3>{name}</h3>
+
+      <img src={image} alt="Without img" />
+      <h4>Types</h4>
+      <div className='types'>
+        { 
+        types.map((type) => {
+            i++;
+            return<h5 className="type" key={id*i}>{type}</h5>
+        }
+        )}
+      </div>
     </div>
-)
+  );
 }
