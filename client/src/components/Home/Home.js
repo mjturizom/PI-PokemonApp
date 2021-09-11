c8dd4d14fde74c7681339327467d54ae234db90b
mjturizom <marlonturizom@gmail.com>
2021-09-11 00:33:49 -0500

    Restructuring modeling component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,56 @@
 import React from "react";
+//importo Hooks
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+//importo Action
+import { getPokemons } from "../../actions";
+import { getTypes } from '../../actions'
+//importo componentes
+import Card from "../Card/Card";
+import Navbar from "../Navbar/Navbar";
+//importo styles
+import "./Home.css";
 
-import './Home.css'
 
-export default function LandingPage(){
-    return (
-        <h1>Pokemons</h1>
-    )
+export default function Home() {
+  const dispatch = useDispatch();
+  const pokemons = useSelector((state) => {
+    return state && state.pokemons;
+  });
+  const types = useSelector((state) => {
+    return state && state.types;
+  });
+  useEffect(() => {
+    dispatch(getPokemons());
+    dispatch(getTypes())
+  }, [dispatch]);
+
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(getPokemons());
+  }
+  return (
+    <>
+    <h1>PokeApp</h1>{
+        types&&<Navbar types={types} handleClick={handleClick} />
+    }
+
+      <div className="cardSpace">
+        {pokemons &&
+          pokemons.map((pokemon) => {
+            return pokemon.map((pokemon) => {
+              return (
+                <Card
+                  key={pokemon.id}
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  image={pokemon.imagen}
+                  types={pokemon.types}
+                />
+              );
+            });
+          })}
+      </div>
+    </>
+  );
 }
