f5fd252223258c690ff8d8704e7c6d50ea33209d
mjturizom <marlonturizom@gmail.com>
2021-09-12 12:29:47 -0500

    Creating filter's and order actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//Accion de traer todos los pokemones 
 export function getPokemons() {
   return function (dispatch) {
     return axios.get("http://localhost:3001/pokemons").then((pokemon) => 
@@ -7,6 +7,7 @@ export function getPokemons() {
     );
   };
 }
+//Accion de traer typos desde DB
 export function getTypes() {
     return function (dispatch) {
       return axios.get("http://localhost:3001/types").then((types) => 
@@ -14,25 +15,44 @@ export function getTypes() {
       );
     };
   }
-export function getPokemon(pokemon) {
-  return function (dispatch) {
-    return fetch("http://localhost:3001/pokemons?name=" + pokemon)
-      .then((pokemon) => pokemon.json())
-      .then((pokemonJson) => {
-        dispatch({ type: "GET_POKEMON", payload: pokemonJson });
-      });
-  };
+//Acccion de filtrado de pokemons por tipo
+export function filterByType(type){
+    return {
+        type:'FILTER_BY_TYPE',
+        payload:type
+    }
 }
-export function getPokemonDetail(id) {
-  return function (dispatch) {
-    return fetch("http://localhost:3001/pokemons/" + id)
-      .then((pokemonDetail) => pokemonDetail.json())
-      .then((pokemonDetailJson) => {
-        dispatch({ type: "GET_POKEMON", payload: pokemonDetailJson });
-      });
-  };
+export function filterByOrigin(type){
+  return {
+      type:'FILTER_BY_ORIGIN',
+      payload:type
+  }
 }
+export function OrderByAZ(orderType){
+  return {
+      type:'ORDER_BY_AZ',
+      payload:orderType
+  }
+}
+// export function getPokemon(pokemon) {
+//   return function (dispatch) {
+//     return fetch("http://localhost:3001/pokemons?name=" + pokemon)
+//       .then((pokemon) => pokemon.json())
+//       .then((pokemonJson) => {
+//         dispatch({ type: "GET_POKEMON", payload: pokemonJson });
+//       });
+//   };
+// }
+// export function getPokemonDetail(id) {
+//   return function (dispatch) {
+//     return fetch("http://localhost:3001/pokemons/" + id)
+//       .then((pokemonDetail) => pokemonDetail.json())
+//       .then((pokemonDetailJson) => {
+//         dispatch({ type: "GET_POKEMON", payload: pokemonDetailJson });
+//       });
+//   };
+// }
 
-export function addPokemon(pokemon) {
-  return { type: "ADD_POKEMON", payload: pokemon };
-}
+// export function addPokemon(pokemon) {
+//   return { type: "ADD_POKEMON", payload: pokemon };
+// }
