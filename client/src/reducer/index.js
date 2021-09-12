ee5b3f284b6d5a81d64449b5192c2e168f154036
mjturizom <marlonturizom@gmail.com>
2021-09-12 12:38:19 -0500

    Adding functionality to filter's actions

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,5 +1,7 @@
 const initialState = {
   pokemons: [],
+  pokemonsToFilterByType:[],
+  pokemonsToFilterByOrigin:[],
   types:[]
 };
 function rootReducer(state = initialState, action) {
@@ -7,6 +9,8 @@ function rootReducer(state = initialState, action) {
     return {
       ...state,
       pokemons: action.payload,
+      pokemonsToFilterByType:action.payload,
+      pokemonsToFilterByOrigin:action.payload
     };
   }
   if (action.type === "GET_TYPES") {
@@ -14,6 +18,27 @@ function rootReducer(state = initialState, action) {
       ...state,
       types: action.payload,
     };
+  }
+  if (action.type==="FILTER_BY_TYPE"){
+    const pokemonsByType=
+    action.payload==='allType' ? state.pokemonsToFilterByType :
+    state.pokemonsToFilterByType.filter((pokemon)=>pokemon.types.includes(action.payload))
+    return{
+        ...state,
+        pokemons:pokemonsByType,
+        pokemonsToFilterByOrigin:pokemonsByType
+    }
+  }
+  if(action.type==="FILTER_BY_ORIGIN"){
+    const pokemonsByOrigin=(
+    action.payload==='all' ? state.pokemonsToFilterByOrigin :
+    action.payload==='creat'?
+    state.pokemonsToFilterByOrigin.filter((pokemon)=>pokemon.id>3000&&pokemon.id<10000):
+    state.pokemonsToFilterByOrigin.filter((pokemon)=>pokemon.id<3000||pokemon.id>10000));
+    return{
+        ...state,
+        pokemons:pokemonsByOrigin
+    }
   }
 }
 export default rootReducer;
