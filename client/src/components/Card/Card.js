575dd54c9738d118edd7a484d544cba4b70ffa7b
mjturizom <marlonturizom@gmail.com>
2021-09-14 02:20:03 -0500

    configurando filtros

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./Card.css";
 
 export default function Card({ id, name, image, types }) {
-    let i=0;
+  let i = 0;
+
   return (
     <div className="card">
       <h3>{name}</h3>
-
-      <img src={image} alt="Without img" />
+      <img srcSet={image} alt="No img" />
       <h4>Types</h4>
-      <div className='types'>
-        { 
-        types.map((type) => {
-            i++;
-            return<h5 className="type" key={id*i}>{type}</h5>
-        }
-        )}
+      <div className="types">
+        {types.map((type) => {
+          i++;
+          return (
+            <h5 className="type" key={id * i}>
+              {type}
+            </h5>
+          );
+        })}
       </div>
     </div>
   );
