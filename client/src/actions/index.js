745c09038d17baeee78ebf96bffc6d25509a3751
mjturizom <marlonturizom@gmail.com>
2021-09-14 02:02:40 -0500

    Agregando acciones y llevandolas a reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,48 +1,49 @@
 import axios from "axios";
-//Accion de traer todos los pokemones 
+//Accion de traer todos los pokemones
 export function getPokemons() {
   return function (dispatch) {
-    return axios.get("http://localhost:3001/pokemons").then((pokemon) => 
-      dispatch({ type: "GET_POKEMONS", payload: pokemon.data })
-    );
+    return axios
+      .get("http://localhost:3001/pokemons")
+      .then((pokemon) =>
+        dispatch({ type: "GET_POKEMONS", payload: pokemon.data })
+      );
   };
 }
 //Accion de traer typos desde DB
 export function getTypes() {
-    return function (dispatch) {
-      return axios.get("http://localhost:3001/types").then((types) => 
-        dispatch({ type: "GET_TYPES", payload: types.data })
-      );
-    };
-  }
+  return function (dispatch) {
+    return axios
+      .get("http://localhost:3001/types")
+      .then((types) => dispatch({ type: "GET_TYPES", payload: types.data }));
+  };
+}
 //Acccion de filtrado de pokemons por tipo
-export function filterByType(type){
-    return {
-        type:'FILTER_BY_TYPE',
-        payload:type
-    }
-}
-export function filterByOrigin(type){
+export function filterByType(type) {
   return {
-      type:'FILTER_BY_ORIGIN',
-      payload:type
-  }
+    type: "FILTER_BY_TYPE",
+    payload: type,
+  };
 }
-export function OrderByAZ(orderType){
+export function filterByOrigin(type) {
   return {
-      type:'ORDER_BY_AZ',
-      payload:orderType
-  }
+    type: "FILTER_BY_ORIGIN",
+    payload: type,
+  };
 }
-// export function getPokemon(pokemon) {
-//   return function (dispatch) {
-//     return fetch("http://localhost:3001/pokemons?name=" + pokemon)
-//       .then((pokemon) => pokemon.json())
-//       .then((pokemonJson) => {
-//         dispatch({ type: "GET_POKEMON", payload: pokemonJson });
-//       });
-//   };
-// }
+export function orderBy(orderType, way) {
+  return {
+    type: "ORDER_BY",
+    payload: orderType,
+    way,
+  };
+}
+export function getPokemon(pokemon) {
+  return function (dispatch) {
+    return axios("http://localhost:3001/pokemons?name=" + pokemon)
+      .then((pokemon) =>  dispatch({ type: "GET_POKEMON", payload: pokemon.data }))
+      .catch((resp)=>dispatch({ type: "GET_POKEMON", payload: [] }));
+  };
+}
 // export function getPokemonDetail(id) {
 //   return function (dispatch) {
 //     return fetch("http://localhost:3001/pokemons/" + id)
@@ -53,6 +54,9 @@ export function OrderByAZ(orderType){
 //   };
 // }
 
-// export function addPokemon(pokemon) {
-//   return { type: "ADD_POKEMON", payload: pokemon };
-// }
+export function addPokemon(pokemon) {
+  return function (dispatch) {
+    return axios.post("http://localhost:3001/pokemons",pokemon)
+      .then((pokemon) =>  dispatch({ type: "ADD_POKEMON", payload: pokemon.data }) );
+  };
+}
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,44 +1,107 @@
 const initialState = {
   pokemons: [],
-  pokemonsToFilterByType:[],
-  pokemonsToFilterByOrigin:[],
-  types:[]
+  pokemonsToFilterByType: [],
+  pokemonsToFilterByOrigin: [],
+  types: [],
 };
+//funciones para ordenar por name
+function sortBynameAsc(a, b) {
+  if (a.name > b.name) return 1;
+  if (a.name < b.name) return -1;
+  return 0;
+}
+function sortBynameDesc(a, b) {
+  if (a.name > b.name) return -1;
+  if (a.name < b.name) return 1;
+  return 0;
+}
+//funciones para ordenar por fuerza
+function sortByFuerzaAsc(a, b) {
+  if (a.fuerza > b.fuerza) return 1;
+  if (a.fuerza < b.fuerza) return -1;
+  return 0;
+}
+function sortByFuerzaDesc(a, b) {
+  if (a.fuerza > b.fuerza) return -1;
+  if (a.fuerza < b.fuerza) return 1;
+  return 0;
+}
 function rootReducer(state = initialState, action) {
   if (action.type === "GET_POKEMONS") {
     return {
       ...state,
       pokemons: action.payload,
-      pokemonsToFilterByType:action.payload,
-      pokemonsToFilterByOrigin:action.payload
+      pokemonsToFilterByType: action.payload,
+      pokemonsToFilterByOrigin: action.payload,
     };
   }
+
+  if(action.type==='GET_POKEMON'){
+    return {
+        ...state,
+        pokemons:action.payload
+    }
+  }
+
   if (action.type === "GET_TYPES") {
     return {
       ...state,
       types: action.payload,
     };
   }
-  if (action.type==="FILTER_BY_TYPE"){
-    const pokemonsByType=
-    action.payload==='allType' ? state.pokemonsToFilterByType :
-    state.pokemonsToFilterByType.filter((pokemon)=>pokemon.types.includes(action.payload))
-    return{
-        ...state,
-        pokemons:pokemonsByType,
-        pokemonsToFilterByOrigin:pokemonsByType
-    }
+  if (action.type === "ADD_POKEMON") {
+    return {
+      ...state,
+      
+    };
   }
-  if(action.type==="FILTER_BY_ORIGIN"){
-    const pokemonsByOrigin=(
-    action.payload==='all' ? state.pokemonsToFilterByOrigin :
-    action.payload==='creat'?
-    state.pokemonsToFilterByOrigin.filter((pokemon)=>pokemon.id>3000&&pokemon.id<10000):
-    state.pokemonsToFilterByOrigin.filter((pokemon)=>pokemon.id<3000||pokemon.id>10000));
-    return{
-        ...state,
-        pokemons:pokemonsByOrigin
-    }
+  if (action.type === "FILTER_BY_TYPE") {
+    const pokemonsByType =
+      action.payload === "allType"
+        ? state.pokemonsToFilterByType
+        : state.pokemonsToFilterByType.filter((pokemon) =>
+            pokemon.types.includes(action.payload)
+          );
+    return {
+      ...state,
+      pokemons: pokemonsByType,
+      pokemonsToFilterByOrigin: pokemonsByType,
+    };
+  }
+  if (action.type === "FILTER_BY_ORIGIN") {
+    const pokemonsByOrigin =
+      action.payload === "all"
+        ? state.pokemonsToFilterByOrigin
+        : action.payload === "creat"
+        ? state.pokemonsToFilterByOrigin.filter(
+            (pokemon) => pokemon.id > 3000 && pokemon.id < 10000
+          )
+        : state.pokemonsToFilterByOrigin.filter(
+            (pokemon) => pokemon.id < 3000 || pokemon.id > 10000
+          );
+    return {
+      ...state,
+      pokemons: pokemonsByOrigin,
+    };
+  }
+  if (action.type === "ORDER_BY") {
+    const pokemonsOrderBy =
+      action.payload === "default"
+        ? action.way === "asc"
+          ? state.pokemons.sort((a, b) => a.id - b.id)
+          : state.pokemons.sort((a, b) => b.id - a.id)
+        : action.payload === "AZ"
+        ? state.pokemons.sort(
+            action.way === "asc" ? sortBynameAsc : sortBynameDesc
+          )
+        : state.pokemons.sort(
+            action.way === "asc" ? sortByFuerzaAsc : sortByFuerzaDesc
+          );
+    return {
+      ...state,
+      pokemons: pokemonsOrderBy,
+    };
   }
 }
+
 export default rootReducer;
