7a14eb02f84a8ab8a66309f572cf80483b1424f3
mjturizom <marlonturizom@gmail.com>
2021-09-14 02:23:08 -0500

    Refactoring

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,36 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import "./Navbar.css";
 
-export default function Home({ types, handleClick, filterByType, filterByOrigin }) {
+export default function Navbar({
+  types,
+  handleClick,
+  filterByType,
+  filterByOrigin,
+  orderBy,
+  orderWay,
+}) {
   return (
-    <div className='navbar'>
-      
+    <div className="navbar">
       <h3>Filter by: Type</h3>
-      <select name="Type" onChange={(e)=>{filterByType(e.target.value)}}>
-      <option value='allType'>All</option>
+      <select
+        
+        name="Type"
+        onChange={(e) => {
+          filterByType(e.target.value);
+        }}
+      >
+        <option value="allType" >All</option>
         {types.map((type) => {
-          return <option key={type.id} value={type.name}>{type.name}</option>;
+          return (
+            <option key={type.id} value={type.name}>
+              {type.name}
+            </option>
+          );
         })}
       </select>
-      <form onChange={(e)=>{filterByOrigin(e.target.value)}}>
-      <input type="radio" name="origin" value="all" />
-      All
-      <input type="radio" name="origin" value="exis" />
-      Existing
-      <input type="radio" name="origin" value="creat" />
-      Created
-     </form>
+      <form
+        onChange={(e) => {
+          filterByOrigin(e.target.value);
+        }}
+      >
+        <input type="radio" onChange={(e) => {
+          filterByOrigin(e.target.value);
+        }} defaultChecked  name="origin" value="all" />
+        All
+        <input type="radio" name="origin" value="exis" />
+        Existing
+        <input type="radio" name="origin" value="creat" />
+        Created
+      </form>
       <h3>Order by</h3>
-      <select>
-      <option value='allOrder'>All</option>
-        <option value='AZ'>A-Z</option>
-        <option value='attack'>Attack</option>
-      </select>{" "}
-      <input type="radio" name="order" value="asc" />
-      Asc
-      <input type="radio" name="order" value="des" />
-      Desc
+      <select
+        id="orderBy"
+        onChange={(e) => {
+          orderBy(e.target.value);
+        }}
+      >
+        <option value="default">Default</option>
+        <option value="AZ">A-Z</option>
+        <option value="attack">Attack</option>
+      </select>
+      <form
+        id="way"
+        onChange={(e) => {
+          orderWay(e.target.value);
+        }}
+      >
+        <input onChange={(e) => {
+          orderWay(e.target.value);
+        }} type="radio" defaultChecked name="order" value="asc" />
+        Asc
+        <input type="radio" name="order" value="des" />
+        Desc
+      </form>
       <button
         onClick={(e) => {
           handleClick(e);
@@ -38,7 +74,9 @@ export default function Home({ types, handleClick, filterByType, filterByOrigin 
       >
         Reset
       </button>
-      <Link className='button' to="/pokemon">Crear Pokemon</Link>
+      <Link className="button" to="/newpokemon">
+        Crear Pokemon
+      </Link>
     </div>
   );
 }
