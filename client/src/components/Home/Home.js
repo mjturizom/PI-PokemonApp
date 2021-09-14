adc21b3943bdfd03cb6e2467b804b26360f35cbf
mjturizom <marlonturizom@gmail.com>
2021-09-14 02:04:48 -0500

    agregando spinner y probando estilos

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,12 +3,20 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 //importo Action
-import { getPokemons,getTypes,filterByType, filterByOrigin } from "../../actions";
+import {
+  getPokemons,
+  getTypes,
+  filterByType,
+  filterByOrigin,
+  orderBy,
+} from "../../actions";
 
 //importo componentes
 import Card from "../Card/Card";
 import Navbar from "../Navbar/Navbar";
 import Page from "../page/Page";
+import SearchBar from "../SearchBar/SearchBar"
+import Spinner from "../Spinner/Spinner";
 //importo styles
 import "./Home.css";
 
@@ -22,8 +30,9 @@ export default function Home() {
   });
 
   const [actualPag, setActualPag] = useState(1);
+  const [orden, setOrden] = useState("");
   //const [numPokemonsInPag, setNumPokemonsInPag] = useState(12);
-  const numPokemonsInPag=12;
+  const numPokemonsInPag = 12;
   const lastCard = actualPag * numPokemonsInPag;
   const firstCard = lastCard - numPokemonsInPag;
   let pokemonsToPage = pokemons;
@@ -49,21 +58,57 @@ export default function Home() {
     dispatch(getPokemons());
   }
   function FilterByType(type) {
-    
     dispatch(filterByType(type));
   }
   function FilterByOrigin(origin) {
-    
     dispatch(filterByOrigin(origin));
   }
+  function OrderBy(orderType) {
+    const tags = document.getElementById("way").children;
+    let isOneChecked = false;
+    for (const tag of tags) {
+      if (tag.checked) {
+        isOneChecked = tag.checked;
+        dispatch(orderBy(orderType, tag.value));
+        break;
+      }
+    }
+    if (!isOneChecked) dispatch(orderBy(orderType, "asc"));
+    setOrden("Ordenado" + orderType);
+    console.log(orden);
+  }
+  function OrderWay(orderWay) {
+    let orderType = "";
+    console.log("seleccion:", orderWay);
+    const tags = document.getElementById("orderBy").children;
+
+    for (const tag of tags) {
+      if (tag.selected) orderType = tag.value;
+    }
+    OrderBy(orderType);
+    setOrden("Ordenado" + orderWay);
+  }
+
   return (
     <>
+    <div className='header'>
       <h1>PokeApp</h1>
-      {types && <Navbar types={types} handleClick={handleClick} filterByType={FilterByType} filterByOrigin={FilterByOrigin}/>}
+      <SearchBar/>
+      </div>
+      {types && (
+        <Navbar
+          types={types}
+          handleClick={handleClick}
+          filterByType={FilterByType}
+          filterByOrigin={FilterByOrigin}
+          orderBy={OrderBy}
+          orderWay={OrderWay}
+        />
+      )}
 
       <div className="cardSpace">
-        {pokemons &&
-          pokemonsToPage.map((pokemon) => {
+        {pokemons?
+          (pokemons.length? pokemonsToPage.map((pokemon) => {
             return (
               <Card
                 key={pokemon.id}
@@ -73,7 +118,7 @@ export default function Home() {
                 types={pokemon.types}
               />
             );
-          })}
+          }):<span>No se encuentra el pokemon, intenta otro nombre</span>):<Spinner/>}
       </div>
       {pokemons && <Page nPages={nPages} Page={page} />}
     </>
